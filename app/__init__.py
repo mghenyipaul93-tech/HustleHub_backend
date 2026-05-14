8b999153eaa92e3542b0a4469d1b517dc0b1d3d2
Yasmin101-101 <yasminmohamudyasmin@gmail.com>
2026-05-14 10:43:46 +0300

    HU-32 Configure PostgreSQL and SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+db = SQLAlchemy()
+migrate = Migrate()
+
 def create_app():
     app = Flask(__name__)
 
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     @app.get("/")
     def index():
